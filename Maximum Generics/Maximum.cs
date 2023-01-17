be27a5394f4736c504e23062ca5d98fc6cb53a17
gitakshay1 <akshaypatil309p8@gmail.com>
2023-01-17 18:43:09 +0530

    [Akshay] Refactor program by using genric method

--- Maximum Generics/Maximum.cs
+++ Maximum Generics/Maximum.cs
@@ -8,7 +8,7 @@ namespace Maximum_Generics
 {
     internal class Maximum
     {
-        public void MaximumInt(int firstValue, int secondValue, int thirdValue)
+        public void Max<T>(T firstValue, T secondValue, T thirdValue)where T:IComparable
         {
             if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0 ||
                 firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) > 0 ||
@@ -28,45 +28,6 @@ namespace Maximum_Generics
             }
 
         }
-        public void Maximumfloat(double firstValue, double secondValue, double thirdValue)
-        {
-            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0 ||
-                firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) > 0 ||
-                firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) >= 0)
-            {
-                Console.WriteLine(firstValue + " is Maximum");
-            }
-            if (secondValue.CompareTo(thirdValue) > 0 && secondValue.CompareTo(firstValue) > 0 ||
-                secondValue.CompareTo(thirdValue) >= 0 && secondValue.CompareTo(firstValue) > 0 ||
-                secondValue.CompareTo(thirdValue) > 0 && secondValue.CompareTo(firstValue) >= 0)
-            {
-                Console.WriteLine(secondValue + " is Maximum");
-            }
-            else
-            {
-                Console.WriteLine(thirdValue + " is Maximum");
-            }
-
-        }
-        public void Maximumstring(string firstValue, string secondValue, string thirdValue)
-        {
-            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0 ||
-                firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) > 0 ||
-                firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) >= 0)
-            {
-                Console.WriteLine(firstValue + " is Maximum");
-            }
-            if (secondValue.CompareTo(thirdValue) > 0 && secondValue.CompareTo(firstValue) > 0 ||
-                secondValue.CompareTo(thirdValue) >= 0 && secondValue.CompareTo(firstValue) > 0 ||
-                secondValue.CompareTo(thirdValue) > 0 && secondValue.CompareTo(firstValue) >= 0)
-            {
-                Console.WriteLine(secondValue + " is Maximum");
-            }
-            else
-            {
-                Console.WriteLine(thirdValue + " is Maximum");
-            }
-
-        }
+        
     }
 }
--- Maximum Generics/Program.cs
+++ Maximum Generics/Program.cs
@@ -5,9 +5,9 @@
         static void Main(string[] args)
         {
             Maximum max=new Maximum();
-            max.MaximumInt(7, 39, 98);
-            max.Maximumfloat(12.33, 36.89, 22.68);
-            max.Maximumstring("Apple", "Peach", "Banana");
+            max.Max(7, 39, 98);
+            max.Max(12.33, 36.89, 22.68);
+            max.Max("Apple", "Peach", "Banana");
         }
     }
 }
