f2bd9893d27f4c7af4fe62eeeebda0f340237082
gitakshay1 <akshaypatil309p8@gmail.com>
2023-01-17 18:36:51 +0530

    [Akshay] Added program to find maximum string value

--- Maximum Generics/Maximum.cs
+++ Maximum Generics/Maximum.cs
@@ -48,5 +48,25 @@ namespace Maximum_Generics
             }
 
         }
+        public void Maximumstring(string firstValue, string secondValue, string thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0 ||
+                firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) > 0 ||
+                firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) >= 0)
+            {
+                Console.WriteLine(firstValue + " is Maximum");
+            }
+            if (secondValue.CompareTo(thirdValue) > 0 && secondValue.CompareTo(firstValue) > 0 ||
+                secondValue.CompareTo(thirdValue) >= 0 && secondValue.CompareTo(firstValue) > 0 ||
+                secondValue.CompareTo(thirdValue) > 0 && secondValue.CompareTo(firstValue) >= 0)
+            {
+                Console.WriteLine(secondValue + " is Maximum");
+            }
+            else
+            {
+                Console.WriteLine(thirdValue + " is Maximum");
+            }
+
+        }
     }
 }
--- Maximum Generics/Program.cs
+++ Maximum Generics/Program.cs
@@ -7,6 +7,7 @@
             Maximum max=new Maximum();
             max.MaximumInt(7, 39, 98);
             max.Maximumfloat(12.33, 36.89, 22.68);
+            max.Maximumstring("Apple", "Peach", "Banana");
         }
     }
 }
