7e5b83826c68295d5ffbcb65cf23fd84253c06cd
gitakshay1 <akshaypatil309p8@gmail.com>
2023-01-17 18:27:27 +0530

    [Akshay] Added program to find maximum integer

---
+++ Maximum Generics/Maximum.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maximum_Generics
+{
+    internal class Maximum
+    {
+        public void MaximumInt(int firstValue, int secondValue, int thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0 ||
+                firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) > 0 ||
+                firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) >= 0)
+            {
+                Console.WriteLine(firstValue+" is Maximum");
+            }
+            if (secondValue.CompareTo(thirdValue) > 0 && secondValue.CompareTo(firstValue) > 0 ||
+                secondValue.CompareTo(thirdValue) >= 0 && secondValue.CompareTo(firstValue) > 0 ||
+                secondValue.CompareTo(thirdValue) > 0 && secondValue.CompareTo(firstValue) >= 0)
+            {
+                Console.WriteLine(secondValue + " is Maximum");
+            }
+            else
+            {
+                Console.WriteLine(thirdValue + " is Maximum");
+            }
+
+        }
+    }
+}
---
+++ Maximum Generics/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Maximum_Generics
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Maximum max=new Maximum();
+            max.MaximumInt(7, 39, 98);
+        }
+    }
+}
